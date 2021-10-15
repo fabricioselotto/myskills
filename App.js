42d6d5799c5b182f4496a3220241a613947eb9e3
fabricioselotto <fabricioselottohard01@gmail.com>
2021-10-15 14:46:45 -0300

    ep1.1

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { StatusBar } from 'react-native'
+
+import { Home } from './src/pages/Home'
 
 export default function App() {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>React Native</Text>
-      <Text>Ignite</Text>
-    </View>
+    <>
+      <StatusBar barStyle='light-content' />
+      <Home />
+    </>
   )
 }
